da81ed80363d0031ede61f13c06a7551611ef9f6
Fityan <fityannugroho@gmail.com>
2022-09-20 19:28:56 +0700

    Initial login & logout endpoint
    And protect the resources endpoint with middleware

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use App\Traits\ApiResponser;
+use Illuminate\Support\Carbon;
+
+class AuthController extends Controller
+{
+    use ApiResponser;
+
+    /**
+     * Login handler
+     *
+     * @param \App\Http\Requests\LoginRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(LoginRequest $request)
+    {
+        $validated = $request->validated();
+        $credentials = [
+            'email' => $validated['email'],
+            'password' => $validated['password'],
+        ];
+
+        if (!auth()->attempt($credentials)) {
+            return $this->fail('Unauthorized', 401);
+        }
+
+        $user = $request->user();
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        return $this->success([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'expires_at' => Carbon::now()->addMinutes(config('sanctum.expiration'))->toDateTimeString()
+        ], 'Login successful', 201);
+    }
+
+    /**
+     * Logout handler
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        $user = User::find(auth()->user()->id);
+        $isDeleted = $user->tokens()->delete();
+
+        if ($isDeleted) {
+            return $this->success(null, 'Logout successful', 200);
+        }
+
+        return $this->fail('Failed to logout', 500);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('albums', 'App\Http\Controllers\Api\AlbumController');
+Route::post('login', [AuthController::class, 'login']);
 
-Route::apiResource('songs', 'App\Http\Controllers\Api\SongController');
+Route::post('logout', [AuthController::class, 'logout'])
+    ->middleware('auth:sanctum');
+
+Route::apiResource('albums', 'App\Http\Controllers\Api\AlbumController')
+    ->middleware(['auth:sanctum']);
+
+Route::apiResource('songs', 'App\Http\Controllers\Api\SongController')
+    ->middleware(['auth:sanctum']);
