77acc826e56596adbc573849b0dd445fd9ec5fed
Fityan <fityannugroho@gmail.com>
2022-09-23 14:28:59 +0700

    Update detail album page

--- resources/views/pages/album/show.blade.php
+++ resources/views/pages/album/show.blade.php
@@ -4,9 +4,20 @@
 
 <x-wrapper>
     <h1>{{ $album->name }} ({{ $album->year }})</h1>
-    <hr>
-    {{-- Song List --}}
 
+    {{-- Edit & Delete Button --}}
+    <div class="d-flex gap-2">
+        <a href="{{ route('songs.create', ['id' => $album]) }}" class="btn btn-primary">Add Song</a>
+        <a href="{{ route('albums.edit', $album) }}" class="btn btn-outline-secondary">Edit</a>
+        <form action="{{ route('albums.destroy', $album) }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-outline-danger">Delete</button>
+        </form>
+    </div>
+    <hr>
+
+    {{-- Song List --}}
     <div class="row">
         <div class="col-12">
             <div class="list-group">
@@ -17,7 +28,7 @@
                         <p class="mb-1">{{ $song->artist }}</p>
                     </div>
                     <div class="text-end">
-                        <p class="fs-5 mb-1">{{ date('i:s', $song->duration) }}</p>
+                        <p class="mb-1" title="Duration">{{ date('i:s', $song->duration) }}</p>
                     </div>
                 </a>
                 @endforeach
